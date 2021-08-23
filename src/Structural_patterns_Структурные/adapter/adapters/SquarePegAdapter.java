19382217de6775f739844c665a52bdc02e0db8c4
AnnaEleeva <anna_eleeva@yahoo.com>
2021-08-23 14:01:09 +0500

    add adapter

---
+++ src/Structural_patterns_Структурные/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,24 @@
+package Structural_patterns_Структурные.adapter.adapters;
+
+
+import Structural_patterns_Структурные.adapter.round.RoundPeg;
+import Structural_patterns_Структурные.adapter.square.SquarePeg;
+
+/**
+ * Адаптер позволяет использовать КвадратныеКолышки и КруглыеОтверстия вместе.
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Рассчитываем минимальный радиус, в который пролезет этот колышек.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
